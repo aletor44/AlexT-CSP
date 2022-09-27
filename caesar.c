532274f8d4582afa3836fccfbd14e1bf003930ac
aletor44 <62729986+aletor44@users.noreply.github.com>
2022-09-26 18:04:39 -0700

    Create caesar.c

---
+++ caesar.c
@@ -0,0 +1,42 @@
+// Alexadner Torjyan 28 Period 1
+// CS50 week 2 problem set 2
+
+using System;
+using System.Text;
+ 
+public class CaesarCipher
+{
+    // Code text
+    public static StringBuilder encrypt(String text, int s)
+    {
+        StringBuilder result= new StringBuilder();
+ 
+        for (int i=0; i<text.Length; i++)
+        {
+            if (char.IsUpper(text[i]))
+            {
+                char ch = (char)(((int)text[i] +
+                                s - 65) % 26 + 65);
+                result.Append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)text[i] +
+                                s - 97) % 26 + 97);
+                result.Append(ch);
+            }
+        }
+        return result;
+    }
+ 
+    // Show 
+    public static void Main(String[] args)
+    {
+        String text = "ATTACKATONCE";
+        int s = 4;
+        Console.WriteLine("Text : " + text);
+        Console.WriteLine("Shift : " + s);
+        Console.WriteLine("Cipher: " + encrypt(text, s));
+    }
+}
+ 
